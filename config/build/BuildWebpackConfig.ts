d56aa5542100a2d92d06dd490f3135a3852433de
Nazar Kuryshchuk <nazarii.kuryshchuk.ri.2023@lpnu.ua>
2024-02-28 00:11:23 +0200

    add react

--- config/build/BuildWebpackConfig.ts
+++ config/build/BuildWebpackConfig.ts
@@ -2,13 +2,14 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import { buildResolvers } from "./buildResolvers";
 import { buildPlugins } from "./buildPlugins";
-import path from "path";
 import { buildLoaders } from "./buildLoaders";
+import { buildDevServer } from "./buildDevServer";
+
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { paths, mode } = options;
+    const { paths, mode, isDev } = options;
     return {
-        mode: mode === "development" ? "development" : "production",
+        mode: mode,
         entry: paths.entry,
         output: {
             filename: "[name].[contenthash].js",
@@ -20,5 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ?  buildDevServer(options) : undefined,
     };
 }
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,24 @@
-import { type } from "os";
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ],
+    }
 
     const typesctiptLoader = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: 'ts-loader',
-    
     }
 
   return [
     typesctiptLoader,
+    cssLoader,
   ];
 }
