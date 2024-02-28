6a775a31c7a116a244effc4a59afe52c057978a2
Nazar Kuryshchuk <nazarii.kuryshchuk.ri.2023@lpnu.ua>
2024-02-28 15:19:07 +0200

    add routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from './components/Counter';
 import './index.scss';
+import {  Link, Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
     return (
         <div className='app'>
-            nfkernfkjebrferj
-            <Counter />
+                <Link to={'/'}>Головна</Link>
+                <Link to={'/about'}>Про сайт</Link>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path={'/'} element={<MainPageAsync />} />
+                        <Route path={'/about'} element={<AboutPageAsync />} />
+                    </Routes>
+                </Suspense>
         </div>
     );
 };
