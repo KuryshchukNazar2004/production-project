5d1ec0e5444e3a6e0dca4978ab4673bf957d73ec
Nazar Kuryshchuk <nazarii.kuryshchuk.ri.2023@lpnu.ua>
2024-03-05 12:45:07 +0200

    navbar and switch theme

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Button.module.scss'
+import { FC } from 'react';
+
+
+export enum ThemeButton{
+    CLEAR = 'clear',
+}
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+    className?: string;
+    theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    
+    const {
+        className,
+        children,
+        theme,
+        ...otherProps
+    } = props;
+    
+    return (
+        <button className={classNames(cls.Button, {[cls[theme]]: true}, [className])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
