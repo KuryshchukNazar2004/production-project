ac27855055f753310a86b1652970f6f62c896195
Nazar Kuryshchuk <nazarii.kuryshchuk.ri.2023@lpnu.ua>
2024-03-05 14:41:23 +0200

    add Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 
 
@@ -15,7 +16,11 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
                 <Navbar />
-                <AppRouter />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+                
         </div>
     );
 };
